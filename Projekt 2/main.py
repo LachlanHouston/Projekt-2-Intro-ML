a4717f76f352a2bd2e71c5d2f22a56679a240101
Frederikravnborg <Frederik@ravnborg.fr>
2022-10-25 15:40:17 +0200

    Update main.py

--- Projekt 2/main.py
+++ Projekt 2/main.py
@@ -1,9 +1,15 @@
 #%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
+
+print(data)
 
 
 #%%
 
-hej
+
 
 #%%
