848f9f308ab479a68fb263a776dce61abb9843af
Lachlan Houston <LachlanHouston111@gmail.com>
2022-10-25 16:18:16 +0200

    Update main.py

--- Projekt 2/main.py
+++ Projekt 2/main.py
@@ -1,11 +1,12 @@
 #%%
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
+from scipy.io import loadmat
+from sklearn import model_selection
 
-data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
-#data = pd.read_csv(r'C:/Users/ssjsi/Documents/GitHub/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
-
+#data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
+data = pd.read_csv (r'C:/Users/Lachl/OneDrive/Documents/Intro ML/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
 
 cullen = data["culmen_length_mm"]
 culdep = data["culmen_depth_mm"]
@@ -18,9 +19,11 @@ culdeps = (culdep-np.mean(culdep))/np.std(culdep)
 flilens = (flilen-np.mean(flilen))/np.std(flilen)
 bodmass = (bodmas-np.mean(bodmas))/np.std(bodmas)
 
-
-
+X = flilen
+y = bodmas
 #%%
+test_proportion = 1/10
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=test_proportion)
 
 
 
