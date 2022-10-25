a4b1464aed7d4fec1ab2f07523ce79ff0f2cede5
Simon Stenbæk Jensen <ssj.simonsj@gmail.com>
2022-10-25 15:57:42 +0200

    Added path for csv file

--- Projekt 2/main.py
+++ Projekt 2/main.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
+#data = pd.read_csv(r'C:/Users/ssjsi/Documents/GitHub/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
+
 
 cullen = data["culmen_length_mm"]
 culdep = data["culmen_depth_mm"]
