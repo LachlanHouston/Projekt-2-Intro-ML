82b42a1226975679a22bf3e148bc8bf492e42372
Lachlan Houston <LachlanHouston111@gmail.com>
2022-10-26 14:27:54 +0200

    Update main.py

--- Projekt 2/main.py
+++ Projekt 2/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib as plt
 from scipy.io import loadmat
 from sklearn.model_selection import cross_val_score
-
+from sklearn import model_selection
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPClassifier
@@ -12,8 +12,8 @@ from sklearn.metrics import mean_squared_error
 
 
 # Load Penguin data
-data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
-# data = pd.read_csv (r'C:/Users/Lachl/OneDrive/Documents/Intro ML/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
+#data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
+data = pd.read_csv (r'C:/Users/Lachl/OneDrive/Documents/Intro ML/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
 
 # Seperate data
 cullen = data["culmen_length_mm"]
@@ -22,48 +22,169 @@ flilen = data["flipper_length_mm"]
 bodmas = data["body_mass_g"]
 
 # Standardize
-cullens = (cullen-np.mean(cullen))/np.std(cullen)
-culdeps = (culdep-np.mean(culdep))/np.std(culdep)
-flilens = (flilen-np.mean(flilen))/np.std(flilen)
-bodmass = (bodmas-np.mean(bodmas))/np.std(bodmas)
+cullen = (cullen-np.mean(cullen))/np.std(cullen)
+culdep = (culdep-np.mean(culdep))/np.std(culdep)
+flilen = (flilen-np.mean(flilen))/np.std(flilen)
+bodmas = (bodmas-np.mean(bodmas))/np.std(bodmas)
 
 # Create X and y, X needs to be reshaped to be used later
-X = np.array([(flilens, culdeps, cullens)]).reshape(-1,3)
-y = np.array(bodmass)
+X = np.array([(flilen, culdep, cullen)]).reshape(-1,3)
+y = np.array(bodmas)
 
 n = len(y)
 
 #%%
 # Define test proportion, and then create k-folds of training and test data
-k = 10
+K = 10
 N_trials = 10
-test_proportion = 1/k
+test_proportion = 1/K
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_proportion, shuffle=True)
 ntest = len(y_test)
+#%%
 # Find baseline (mean of the y training set)
 baseline = np.mean(y_train)
 
 # Compute Mean Squared Error
 baseerror =1/ntest * np.sum(y_test-baseline)**2
-print("Baseline mean squared error: ", baseerror)
-
+print("baseline mean squared error: ", baseerror)
 #%%
-# Define the LinearRegression model
-LRmodel = LinearRegression()
+def rlr_validate(X,y,lambdas,cvf=10):
+    ''' Validate regularized linear regression model using 'cvf'-fold cross validation.
+        Find the optimal lambda (minimizing validation error) from 'lambdas' list.
+        The loss function computed as mean squared error on validation set (MSE).
+        Function returns: MSE averaged over 'cvf' folds, optimal value of lambda,
+        average weight values for all lambdas, MSE train&validation errors for all lambdas.
+        The cross validation splits are standardized based on the mean and standard
+        deviation of the training set when estimating the regularization strength.
+        
+        Parameters:
+        X       training data set
+        y       vector of values
+        lambdas vector of lambda values to be validated
+        cvf     number of crossvalidation folds     
+        
+        Returns:
+        opt_val_err         validation error for optimum lambda
+        opt_lambda          value of optimal lambda
+        mean_w_vs_lambda    weights as function of lambda (matrix)
+        train_err_vs_lambda train error as function of lambda (vector)
+        test_err_vs_lambda  test error as function of lambda (vector)
+    '''
+    CV = model_selection.KFold(cvf, shuffle=True)
+    M = X.shape[1]
+    w = np.empty((M,cvf,len(lambdas)))
+    train_error = np.empty((cvf,len(lambdas)))
+    test_error = np.empty((cvf,len(lambdas)))
+    f = 0
+    y = y.squeeze()
+    for train_index, test_index in CV.split(X,y):
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        
+        # Standardize the training and set set based on training set moments
+        mu = np.mean(X_train[:, 1:], 0)
+        sigma = np.std(X_train[:, 1:], 0)
+        
+        X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+        X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
+        
+        # precompute terms
+        Xty = X_train.T @ y_train
+        XtX = X_train.T @ X_train
+        for l in range(0,len(lambdas)):
+            # Compute parameters for current value of lambda and current CV fold
+            # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
+            lambdaI = lambdas[l] * np.eye(M)
+            lambdaI[0,0] = 0 # remove bias regularization
+            w[:,f,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+            # Evaluate training and test performance
+            train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).mean(axis=0)
+            test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).mean(axis=0)
+    
+        f=f+1
 
-# Define ANN model
-
+    opt_val_err = np.min(np.mean(test_error,axis=0))
+    opt_lambda = lambdas[np.argmin(np.mean(test_error,axis=0))]
+    train_err_vs_lambda = np.mean(train_error,axis=0)
+    test_err_vs_lambda = np.mean(test_error,axis=0)
+    mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
+    
+    return opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
 #%%
-# Fit the model to the training data
-cv_acc = cross_val_score(LRmodel, X, y, cv=k, scoring="neg_mean_squared_error")
+N, M = X.shape
 
-#%%
-LRmodel.fit(X_train, y_train)
+# Add offset attribute
+X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+M = M+1
 
-# Make predictions based on the data in the test set
-predictions = LRmodel.predict(X_test)
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+CV = model_selection.KFold(K, shuffle=True)
+#CV = model_selection.KFold(K, shuffle=False)
 
-#%%
-# Find overall error by finding mean squared error between the predictions and the true labels
-print("Linear Regression mean squared error: ", mean_squared_error(y_test, predictions))
+# Values of lambda
+lambdas = np.power(10.,range(-5,9))
+
+# Initialize variables
+#T = len(lambdas)
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_rlr = np.empty((K,1))
+Error_test_rlr = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
+w_rlr = np.empty((M,K))
+mu = np.empty((K, M-1))
+sigma = np.empty((K, M-1))
+w_noreg = np.empty((M,K))
+
+k = 0
+for train_index, test_index in CV.split(X,y):
+    
+    # extract training and test set for current CV fold
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+    internal_cross_validation = 10    
+    
+    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
+
+    # Standardize outer fold based on training set, and save the mean and standard
+    # deviations since they're part of the model (they would be needed for
+    # making new predictions) - for brevity we won't always store these in the scripts
+    mu[k, :] = np.mean(X_train[:, 1:], 0)
+    sigma[k, :] = np.std(X_train[:, 1:], 0)
+    
+    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+    
+    # Compute mean squared error without using the input data at all
+    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+
+    # Estimate weights for the optimal value of lambda, on entire training set
+    lambdaI = opt_lambda * np.eye(M)
+    lambdaI[0,0] = 0 # Do no regularize the bias term
+    w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+    # Compute mean squared error with regularization with optimal lambda
+    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/len(y_train)
+    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/len(y_test)
+
+    # Estimate weights for unregularized linear regression, on entire training set
+    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
+    # Compute mean squared error without regularization
+    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+    #m = lm.LinearRegression().fit(X_train, y_train)
+    #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+    k += 1
