bbc76d4ed118e09f0caa68f81eae4e18522c11b6
Lachlan Houston <LachlanHouston111@gmail.com>
2022-11-09 14:08:08 +0100

    Update main - Lachlan.py

--- Projekt 2/main - Lachlan.py
+++ Projekt 2/main - Lachlan.py
@@ -10,13 +10,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import mean_squared_error
-
-import torch
-
+from sklearn.naive_bayes import MultinomialNB
 
 # Load Penguin data
-data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
-# data = pd.read_csv (r'C:/Users/Lachl/OneDrive/Documents/Intro ML/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
+#data = pd.read_csv (r'/Users/frederikravnborg/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/DTU-Frederik’s MacBook Pro/ML/Project 1/penguins.csv')
+data = pd.read_csv (r'C:/Users/Lachl/OneDrive/Documents/Intro ML/Projekt-2-Intro-ML/Projekt 2/penguins_size.csv')
 
 # Seperate data
 cullen = data["culmen_length_mm"]
@@ -34,8 +32,12 @@ bodmas = (bodmas-np.mean(bodmas))/np.std(bodmas)
 X = np.array([(flilen, culdep, cullen)]).reshape(-1,3)
 y = np.array(bodmas)
 
-n = len(y)
+# Add offset attribute
+X = np.concatenate((np.ones((X.shape[0],1)),X),1)
 
+N, M = X.shape
+
+n = len(y)
 
 #%%
 def rlr_validate(X,y,lambdas,cvf=10):
@@ -68,14 +70,21 @@ def rlr_validate(X,y,lambdas,cvf=10):
     f = 0
     y = y.squeeze()
     for train_index, test_index in CV.split(X,y):
-        X_train_temp = X[train_index]
-        y_train_temp = y[train_index]
-        X_test_temp = X[test_index]
-        y_test_temp = y[test_index]
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+        
+        # Standardize the training and set set based on training set moments
+        mu = np.mean(X_train[:, 1:], 0)
+        sigma = np.std(X_train[:, 1:], 0)
+        
+        X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+        X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
         
         # precompute terms
-        Xty = X_train_temp.T @ y_train_temp
-        XtX = X_train_temp.T @ X_train_temp
+        Xty = X_train.T @ y_train
+        XtX = X_train.T @ X_train
         for l in range(0,len(lambdas)):
             # Compute parameters for current value of lambda and current CV fold
             # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
@@ -83,8 +92,8 @@ def rlr_validate(X,y,lambdas,cvf=10):
             lambdaI[0,0] = 0 # remove bias regularization
             w[:,f,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
             # Evaluate training and test performance
-            train_error[f,l] = np.power(y_train_temp-X_train_temp @ w[:,f,l].T,2).mean(axis=0)
-            test_error[f,l] = np.power(y_test_temp-X_test_temp @ w[:,f,l].T,2).mean(axis=0)
+            train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).mean(axis=0)
+            test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).mean(axis=0)
     
         f=f+1
 
@@ -96,148 +105,11 @@ def rlr_validate(X,y,lambdas,cvf=10):
     
     return opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
 #%%
-#ANN
-# Define the model
-
-def train_neural_net(model, loss_fn, X, y,
-                     n_replicates=5, max_iter = 100000, tolerance=1e-6):
-    """
-    Train a neural network with PyTorch based on a training set consisting of
-    observations X and class y. The model and loss_fn inputs define the
-    architecture to train and the cost-function update the weights based on,
-    respectively.
-    
-    Usage:
-        Assuming loaded dataset (X,y) has been split into a training and 
-        test set called (X_train, y_train) and (X_test, y_test), and
-        that the dataset has been cast into PyTorch tensors using e.g.:
-            X_train = torch.tensor(X_train, dtype=torch.float)
-        Here illustrating a binary classification example based on e.g.
-        M=2 features with H=2 hidden units:
-    
-        >>> # Define the overall architechture to use
-        >>> model = lambda: torch.nn.Sequential( 
-                    torch.nn.Linear(M, H),  # M features to H hiden units
-                    torch.nn.Tanh(),        # 1st transfer function
-                    torch.nn.Linear(H, 1),  # H hidden units to 1 output neuron
-                    torch.nn.Sigmoid()      # final tranfer function
-                    ) 
-        >>> loss_fn = torch.nn.BCELoss() # define loss to use
-        >>> net, final_loss, learning_curve = train_neural_net(model,
-                                                       loss_fn,
-                                                       X=X_train,
-                                                       y=y_train,
-                                                       n_replicates=3)
-        >>> y_test_est = net(X_test) # predictions of network on test set
-        >>> # To optain "hard" class predictions, threshold the y_test_est
-        >>> See exercise ex8_2_2.py for indepth example.
-        
-        For multi-class with C classes, we need to change this model to e.g.:
-        >>> model = lambda: torch.nn.Sequential(
-                            torch.nn.Linear(M, H), #M features to H hiden units
-                            torch.nn.ReLU(), # 1st transfer function
-                            torch.nn.Linear(H, C), # H hidden units to C classes
-                            torch.nn.Softmax(dim=1) # final tranfer function
-                            )
-        >>> loss_fn = torch.nn.CrossEntropyLoss()
-        
-        And the final class prediction is based on the argmax of the output
-        nodes:
-        >>> y_class = torch.max(y_test_est, dim=1)[1]
-        
-    Args:
-        model:          A function handle to make a torch.nn.Sequential.
-        loss_fn:        A torch.nn-loss, e.g.  torch.nn.BCELoss() for binary 
-                        binary classification, torch.nn.CrossEntropyLoss() for
-                        multiclass classification, or torch.nn.MSELoss() for
-                        regression (see https://pytorch.org/docs/stable/nn.html#loss-functions)
-        n_replicates:   An integer specifying number of replicates to train,
-                        the neural network with the lowest loss is returned.
-        max_iter:       An integer specifying the maximum number of iterations
-                        to do (default 10000).
-        tolerenace:     A float describing the tolerance/convergence criterion
-                        for minimum relative change in loss (default 1e-6)
-                        
-        
-    Returns:
-        A list of three elements:
-            best_net:       A trained torch.nn.Sequential that had the lowest 
-                            loss of the trained replicates
-            final_loss:     An float specifying the loss of best performing net
-            learning_curve: A list containing the learning curve of the best net.
-    
-    """
-    # Specify maximum number of iterations for training
-    logging_frequency = 1000 # display the loss every 1000th iteration
-    best_final_loss = 1e100
-    for r in range(n_replicates):
-        print('\n\tReplicate: {}/{}'.format(r+1, n_replicates))
-        # Make a new net (calling model() makes a new initialization of weights) 
-        net = model()
-        
-        # initialize weights based on limits that scale with number of in- and
-        # outputs to the layer, increasing the chance that we converge to 
-        # a good solution
-        torch.nn.init.xavier_uniform_(net[0].weight)
-        torch.nn.init.xavier_uniform_(net[2].weight)
-                     
-        # We can optimize the weights by means of stochastic gradient descent
-        # The learning rate, lr, can be adjusted if training doesn't perform as
-        # intended try reducing the lr. If the learning curve hasn't converged
-        # (i.e. "flattend out"), you can try try increasing the maximum number of
-        # iterations, but also potentially increasing the learning rate:
-        #optimizer = torch.optim.SGD(net.parameters(), lr = 5e-3)
-        
-        # A more complicated optimizer is the Adam-algortihm, which is an extension
-        # of SGD to adaptively change the learing rate, which is widely used:
-        optimizer = torch.optim.Adam(net.parameters())
-        
-        # Train the network while displaying and storing the loss
-        print('\t\t{}\t{}\t\t\t{}'.format('Iter', 'Loss','Rel. loss'))
-        learning_curve = [] # setup storage for loss at each step
-        old_loss = 1e6
-        for i in range(max_iter):
-            y_est = net(X) # forward pass, predict labels on training set
-            loss = loss_fn(y_est, y) # determine loss
-            loss_value = loss.data.numpy() #get numpy array instead of tensor
-            learning_curve.append(loss_value) # record loss for later display
-            
-            # Convergence check, see if the percentual loss decrease is within
-            # tolerance:
-            p_delta_loss = np.abs(loss_value-old_loss)/old_loss
-            if p_delta_loss < tolerance: break
-            old_loss = loss_value
-            
-            # display loss with some frequency:
-            if (i != 0) & ((i+1) % logging_frequency == 0):
-                print_str = '\t\t' + str(i+1) + '\t' + str(loss_value) + '\t' + str(p_delta_loss)
-                print(print_str)
-            # do backpropagation of loss and optimize weights 
-            optimizer.zero_grad(); loss.backward(); optimizer.step()
-            
-            
-        # display final loss
-        print('\t\tFinal loss:')
-        print_str = '\t\t' + str(i+1) + '\t' + str(loss_value) + '\t' + str(p_delta_loss)
-        print(print_str)
-        
-        if loss_value < best_final_loss: 
-            best_net = net
-            best_final_loss = loss_value
-            best_learning_curve = learning_curve
-        
-    # Return the best curve along with its final loss and learing curve
-    return best_net, best_final_loss, best_learning_curve
-#%%
-N, M = X.shape
-
-# Add offset attribute
-X = np.concatenate((np.ones((X.shape[0],1)),X),1)
-M = M+1
+# K-fold crossvalidation
 
 ## Crossvalidation
 # Create crossvalidation partition for evaluation
-K = 10
+K = 5
 CV = model_selection.KFold(K, shuffle=True)
 #CV = model_selection.KFold(K, shuffle=False)
 
@@ -246,45 +118,18 @@ lambdas = np.power(10.,range(-5,9))
 
 # Initialize variables
 #T = len(lambdas)
-RegError_train = np.empty((K,1))
-RegError_test = np.empty((K,1))
-BaselineError_train = np.empty((K,1))
-BaselineError_test = np.empty((K,1))
-LRError_train = np.empty((K,1))
-LRError_test = np.empty((K,1))
-
-ANNError = []
-ANN_est = 0
-
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_rlr = np.empty((K,1))
+Error_test_rlr = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
 w_rlr = np.empty((M,K))
 mu = np.empty((K, M-1))
 sigma = np.empty((K, M-1))
 w_noreg = np.empty((M,K))
 
-k = 0
-
-# Parameters for neural network classifier
-n_hidden_units = 5      # number of hidden units
-n_replicates = 1        # number of networks trained in each k-fold
-max_iter = 100000
-
-counter = 0
-
-model = lambda: torch.nn.Sequential(
-                    torch.nn.Linear(M, n_hidden_units), #M features to n_hidden_units
-                    torch.nn.Tanh(),   # 1st transfer function,
-                    torch.nn.Linear(n_hidden_units, 1), # n_hidden_units to 1 output neuron
-                    torch.nn.Sigmoid() # final tranfer function
-                    # no final tranfer function, i.e. "linear output"
-                    )
-loss_fn = torch.nn.MSELoss() # notice how this is now a mean-squared-error loss
-
-# Setup figure for display of learning curves and error rates in fold
-summaries, summaries_axes = plt.subplots(1,2, figsize=(10,5))
-
-color_list = ['tab:orange', 'tab:green', 'tab:purple', 'tab:brown', 'tab:pink',
-              'tab:gray', 'tab:olive', 'tab:cyan', 'tab:red', 'tab:blue']
-
+k=0
 for train_index, test_index in CV.split(X,y):
     
     # extract training and test set for current CV fold
@@ -299,80 +144,70 @@ for train_index, test_index in CV.split(X,y):
     # Standardize outer fold based on training set, and save the mean and standard
     # deviations since they're part of the model (they would be needed for
     # making new predictions) - for brevity we won't always store these in the scripts
-    mu[k, :] = np.mean(X_train[:, 1:], 0)
-    sigma[k, :] = np.std(X_train[:, 1:], 0)
-    
-    X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
     
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
     
     # Compute mean squared error without using the input data at all
-    BaselineError_train[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
-    BaselineError_test[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
+    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum(axis=0)/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum(axis=0)/y_test.shape[0]
 
     # Estimate weights for the optimal value of lambda, on entire training set
     lambdaI = opt_lambda * np.eye(M)
     lambdaI[0,0] = 0 # Do no regularize the bias term
     w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
     # Compute mean squared error with regularization with optimal lambda
-    RegError_train[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/len(y_train)
-    RegError_test[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/len(y_test)
+    Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
 
     # Estimate weights for unregularized linear regression, on entire training set
     w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
     # Compute mean squared error without regularization
-    m = lm.LinearRegression().fit(X_train, y_train)
-    LRError_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
-    LRError_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+    Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
+    Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
+    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+    #m = lm.LinearRegression().fit(X_train, y_train)
+    #Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    #Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
 
-    # ANN implementation
-    # Extract training and test set for current CV fold, convert to tensors
-    X_ANN_train = torch.Tensor(X[train_index,:])
-    y_ANN_train = torch.Tensor(y[train_index])
-    X_ANN_test = torch.Tensor(X[test_index,:])
-    y_ANN_test = torch.Tensor(y[test_index])
+    # Display the results for the last cross-validation fold
+    if k == K-1:
+        plt.figure(k, figsize=(12,8))
+        plt.subplot(1,2,1)
+        plt.semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+        plt.xlabel('Regularization factor')
+        plt.ylabel('Mean Coefficient Values')
+        plt.grid()
+        # You can choose to display the legend, but it's omitted for a cleaner 
+        # plot, since there are many attributes
+        #legend(attributeNames[1:], loc='best')
+        
+        plt.subplot(1,2,2)
+        plt.title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+        plt.loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+        plt.xlabel('Regularization factor')
+        plt.ylabel('Squared error (crossvalidation)')
+        plt.legend(['Train error','Validation error'])
+        plt.grid()
+    
+    # To inspect the used indices, use these print statements
+    #print('Cross validation fold {0}/{1}:'.format(k+1,K))
+    #print('Train indices: {0}'.format(train_index))
+    #print('Test indices: {0}\n'.format(test_index))
 
-    # Train the net on training data
-    net, final_loss, learning_curve = train_neural_net(model,
-                                                       loss_fn,
-                                                       X=X_ANN_train,
-                                                       y=y_ANN_train,
-                                                       n_replicates=n_replicates,
-                                                       max_iter=max_iter)
-    
-    print('\n\tBest loss: {}\n'.format(final_loss))
-    
-    # Determine estimated class labels for test set
-    ANN_est = net(X_ANN_test).data.numpy()
-    
-    # Determine errors and errors
-    # se = (y_test_est.float()-y_ANN_test.float())**2 # squared error
-    # Determine errors and errors
-    se = (ANN_est-y_test)**2 # squared error
-    mse = np.mean(sum(se)/len(y_test)) #mean
-    ANNError.append(mse) # store error rate for current CV fold 
+    k+=1
+#%%
+#ANN
+# Define the model
 
-    h, = summaries_axes[0].plot(learning_curve, color=color_list[k])
-    h.set_label('CV fold {0}'.format(k+1))
-    
-    k += 1
+#%%
 
-print(opt_lambda)
-# Spørg mig ikke
 X = np.array([(flilen, culdep, cullen)]).reshape(-1,3)
 y = np.array(bodmas)
 
 #%%
 
-# Display the learning curve for the best net in the current fold
-summaries_axes[0].set_xlabel('Iterations')
-summaries_axes[0].set_xlim((0, 2000))
-summaries_axes[0].set_ylabel('Loss')
-summaries_axes[0].set_title('Learning curves')
 
-plt.show()
 
 #%%
 Tabel = np.array([BaselineError_test[:,0], LRError_test[:,0], ANNError[:,0]])
